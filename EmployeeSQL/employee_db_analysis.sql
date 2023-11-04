916247fe0cfd08581377cf15a7987f2233789266
rabellan <roland.abellano@gmail.com>
2023-11-03 19:40:27 -0700

    Updated the employee_db_analysis AGAIN

--- EmployeeSQL/employee_db_analysis.sql
+++ EmployeeSQL/employee_db_analysis.sql
@@ -1,3 +1,5 @@
+-- Note: I am using aliases for my query to get used to SQL query best practices
+
 -- List the employee number, last name, first name, sex, and salary of each employee.
 -- Result: There are 300024 records from this query
 SELECT
@@ -57,3 +59,63 @@ JOIN
     employee e ON de.emp_no = e.emp_no
 JOIN
     department d ON de.dept_no = d.dept_no;
+
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+-- Result: There are 20 employee records with the first name, "Hercules" and last name starting with "B"
+SELECT
+    first_name,
+    last_name,
+    sex
+FROM
+    employee
+WHERE
+    first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- List each employee in the Sales department, including their employee number, last name, and first name.
+-- Result: There are 52245 records from this query
+SELECT
+    e.emp_no AS sales_employee_number,
+    e.last_name,
+    e.first_name
+FROM
+    employee e
+JOIN
+    dept_emp de ON e.emp_no = de.emp_no
+JOIN
+    department d ON de.dept_no = d.dept_no
+WHERE
+    d.dept_name = 'Sales'
+ORDER BY
+	e.last_name;
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+-- Result: There are 137952 records from this query
+SELECT
+    e.emp_no AS employee_number,
+    e.last_name,
+    e.first_name,
+    d.dept_name AS department_name
+FROM
+    employee e
+JOIN
+    dept_emp de ON e.emp_no = de.emp_no
+JOIN
+    department d ON de.dept_no = d.dept_no
+WHERE
+    d.dept_name IN ('Sales', 'Development')
+ORDER BY
+	d.dept_name ASC;
+
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+-- Resuls: There are 1638 last names shared by multiple employees. Sounds like nepotism. :-)
+SELECT
+    last_name,
+    COUNT(*) AS frequency
+FROM
+    employee
+GROUP BY
+    last_name
+ORDER BY
+    frequency DESC, last_name;
